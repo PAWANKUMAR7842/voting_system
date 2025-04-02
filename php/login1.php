d6770de82110175c32f4753d3faf781c249d8de7
Kothavala Pawan Kumar <pawankothavala7842@gmail.com>
2025-04-02 21:47:00 +0530

    Update login1.php

--- php/login1.php
+++ php/login1.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require __DIR__ . '/vendor/autoload.php'; // Load PHPMailer
+require __DIR__ . '/../vendor/autoload.php';
 
 // Database connection
 $conn = new mysqli("localhost", "root", "", "vote");
